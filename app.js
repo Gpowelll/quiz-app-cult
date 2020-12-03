87371f5d42eb748ffd23e7e77af7d6c7ea887929
Greg Powell <gregtpowell@gmail.com>
2020-12-02 21:34:57 -0600

    finished changes with css

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const store = {
         'Hollywood United'
       ],
       correctAnswer: 'The People\'s Temple',
-      imgSrc: './images/the-peoples-temple.jpg'
+      imgName: 'the-peoples-temple.jpg'
     },
 
     {
@@ -28,7 +28,7 @@ const store = {
         'Heaven\'s Gate'
       ],
       correctAnswer: 'Heaven\'s Gate',
-      imgSrc: './images/heavens-gate.jpeg'
+      imgName: 'heavens-gate.jpeg'
     },
 
     {
@@ -40,7 +40,7 @@ const store = {
         'Heaven\'s gate'
       ],
       correctAnswer: 'The Rajneesh Movement',
-      imgSrc: './images/rajneesh.jpg'
+      imgName: 'rajneesh.jpg'
     },
 
     {
@@ -52,7 +52,7 @@ const store = {
         'The Manson Family'
       ],
       correctAnswer: 'Aum Shinrikyo',
-      imgSrc: './images/aum-shinrikyo-.jpg'
+      imgName: 'aum-shinrikyo.jpg'
     },
 
     {
@@ -64,12 +64,14 @@ const store = {
         'The Manson Family'
       ],
       correctAnswer: 'The Manson Family',
-      imgSrc: './images/manson-family.jpg'
+      imgName: 'manson-family.jpg'
     }
   ],
+  feedbackGiven: true,
   quizStarted: false,
   questionNumber: 0,
-  score: 0
+  score: 0,
+  currentAnswer: ''
 };
 
 /**
@@ -91,77 +93,165 @@ const store = {
 
 // These functions return HTML templates
 
-function generateStartPage() {
+function generateMainPage() {
   return `
     <div class='mainPage'>
+    <img src="./images/clouds.jpg" alt="clouds">
     <h2>Cult Quiz</h2>
     <p>This is a quiz about some of the most notorious cult's in American history.</p>
-    <button type='submit' id='startquiz'>Start Quiz</button>
+    <button type='submit' id='startQuiz'>Start Quiz</button>
     </form>
     </div>`;
 }
 
 
 function generateQuestionPage() {
+  const question = store.questions[store.questionNumber];
+  const answers = question.answers.map(function (answer, index) {
+    return `<input type="radio" id="answer${index}" name="answer" value="${answer}" required>
+    <label for="answer${index}">${answer}</label></br>`;
+  });
+  return `
+  <div class="mainPage">
+    <form id="question">
+      <h2>${question.question}</h2>
+      ${answers.join('')}
+      <button type="form">Submit Answer</button>
+    </form>
 
+    <div class="quiz-info">
+      <p>${store.questionNumber + 1}/5</p>
+      <p>${store.score}/${store.questionNumber} Correct</p>
+    </div>
+  `;
 }
 
-function generateEndPage() {
-
+function generateFeedbackPage() {
+  let feedback = '';
+  if(store.currentAnswer===store.questions[store.questionNumber].correctAnswer) {
+    feedback = 'That is correct!';
+  } else {
+    feedback = 'No, sorry you are incorrect.';
+  }
+  return`
+  <div class="mainPage">
+      <h2>Feedback Question ${store.questionNumber+1}</h2>
+      <img src="images/${store.questions[store.questionNumber].imgName}" alt="${store.questions[store.questionNumber].correctAnswer}">
+      <p>The correct Answer was: ${store.questions[store.questionNumber].correctAnswer}.</p>
+      ${feedback}
+      <p>You have gotten ${store.score}/${store.questionNumber+1} questions right so far.</p>
+      <button type='submit' id="continue">Continue</button>
+      </form>
+    </div>
+    `;
 }
 
-function generateIncorrectPage() {
-
+function generateFinalPage() {
+  let feedback = '';
+  if (store.currentAnswer === store.questions[store.questionNumber].correctAnswer) {
+    feedback = 'That is correct!';
+  } else {
+    feedback = 'No, sorry you are incorrect.';
+  }
+  return`
+  <div class="mainPage">
+      <h2>Feedback Question ${store.questionNumber+1}</h2>
+      <img src="images/${store.questions[store.questionNumber].imgName}" alt="${store.questions[store.questionNumber].correctAnswer}">
+      <p> The correct answer was: ${store.questions[store.questionNumber].correctAnswer}</p>
+      ${feedback}
+      <p>You have completed my quiz!</p>
+      <p>You ended up getting ${store.score}/${store.questionNumber+1} questions right.</p>
+      <button type='submit' id="try-again">Try Again</button>
+      </form>
+    </div>
+    `;
 }
 
 
-
-
-
-
-
-/********** RENDER FUNCTION(S) **********/
-
-// This function conditionally replaces the contents of the <main> tag based on the state of the store
-
-function renderPage(isCorrect) {
-  let html = ''
-
-
-}
-
-
-
-/********** EVENT HANDLER FUNCTIONS **********/
+// EVENT HANDLER FUNCTIONS 
 
 //These functions handle events (submit, click, etc)
 
-function handleBeginClick() {
+function handleStartButton() {
+  $('main').on('click','#startQuiz', function(e) {
+    store.quizStarted = true;
+    renderPage();
+  });
+}
+
+function handleTryAgainButton() {
+  $('main').on('click','#try-again', function (e) {
+    store.currentAnswer = '';
+    store.score = 0;
+    store.questionNumber = 0;
+    store.feedbackGiven = true;
+    store.quizStarted = false;
+    renderPage();
+  });
+}
+
+function handleHomeButton() {
+  $('main').on('click','#home', function(e) {
+    store.currentAnswer = '';
+    store.score = 0;
+    store.questionNumber = 0;
+    store.feedbackGiven = true;
+    store.quizStarted = false;
+    renderPage();
+  });
 
 }
 
-function handleSubmitQuestion() {
-
+function handleSubmitButton() {
+  $('main').on('submit','#question', function(e) {
+    e.preventDefault();
+    // eslint-disable-next-line quotes
+    store.currentAnswer = $(`input[name='answer']:checked`).val();
+    store.feedbackGiven = false;
+    if(store.currentAnswer===store.questions[store.questionNumber].correctAnswer) {
+      store.score++;
+    }
+    renderPage();
+  });
 }
 
-function handleNextQuestion() {
-
+function handleContinueButton(){
+  $('main').on('click','#continue', function(e){
+    store.feedbackGiven = true;
+    store.currentAnswer = '';
+    store.questionNumber++;
+    renderPage();
+  });
 }
 
-function handleTryAgain() {
 
+//RENDER FUNCTION(S)
+
+// This function conditionally replaces the contents of the <main> tag based on the state of the store
+
+function renderPage() {
+  let html = '';
+  if (store.quizStarted === false) {
+    html = generateMainPage();
+  } else if (store.feedbackGiven === true) {
+    html = generateQuestionPage();
+  } else if (store.feedbackGiven === false && store.questionNumber === store.questions.length-1) {
+    html = generateFinalPage();
+  } else{
+    html = generateFeedbackPage();
+  }
+  $('main').html(html);
 }
-
-
 
 
 function main() {
   renderPage();
-  handleSubmitQuestion();
-  handleBeginClick();
-  handleNextQuestion();
-  handleTryAgain();
+  handleStartButton();
+  handleSubmitButton();
+  handleContinueButton();
+  handleTryAgainButton();
+  handleHomeButton();
+  
 }
-
-
-$(main)
+// eslint-disable-next-line no-undef
+$(main);
